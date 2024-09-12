4647a8b5303b5ae37b3ce95c088fed50e15c6ad9
DongYeop <s01054830455@gmail.com>
2024-09-12 20:31:44 +0900

    refactor(test): 패키지 이동

--- src/test/java/calculator/StringSeparatorTest.java
+++ src/test/java/calculator/StringSeparatorTest.java
@@ -1,8 +1,7 @@
-package calculator.primitive;
+package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import calculator.StringSeparator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
