7772933d69a7f68faba757742eda2641a93f8d1d
DongYeop <s01054830455@gmail.com>
2024-09-16 18:30:35 +0900

    refactor(InputView): 메서드 이름 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public String inputRacingCar() {
+    public String inputCarNames() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return sc.nextLine();
     }
 
-    public int inputTryRacingCounts() {
+    public int inputRacingCount() {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("시도할 회수는 몇회인가요?");
