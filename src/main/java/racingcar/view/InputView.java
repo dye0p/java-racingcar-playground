7ae18d4d4c9b94c99b0ceaee81b03dd882db59d5
DongYeop <s01054830455@gmail.com>
2024-09-16 18:12:53 +0900

    feat(InputView): 콘솔 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,20 @@
 package racingcar.view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    public String inputRacingCar() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return sc.nextLine();
+    }
+
+    public int inputTryRacingCounts() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextInt();
+    }
 }
