8bd3687d98194fff397b4eddbe6cff35e2cdcd60
DongYeop <s01054830455@gmail.com>
2024-09-18 18:29:35 +0900

    feat(Racing): 자동차 경주를 담당하는 객체 추가
    - 자동차 경주 시작부터 결과까지 담당하는 책임 부여

--- src/main/java/racingcar/model/Racing.java
+++ src/main/java/racingcar/model/Racing.java
@@ -1,4 +1,47 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.util.RandomNumberCreator;
+
 public class Racing {
+
+    private List<Car> cars;
+    private int racingCount;
+
+    public Racing(String[] carNames, int racingCount) {
+        this.cars = creatCarGroup(carNames);
+        this.racingCount = racingCount;
+    }
+
+    public List<Car> creatCarGroup(String[] carNames) {
+        cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        return cars;
+    }
+
+    public void racing() {
+        for (int i = 0; i < cars.size(); i++) {
+            Car car = cars.get(i);
+            car.move(new RandomNumberCreator().createRandomNubmer());
+        }
+        racingCount--;
+    }
+
+    public boolean isTryRacing() {
+        return racingCount > 0;
+    }
+
+    public List<Car> racingResult() {
+        return cars;
+    }
+
+    public List<Car> winner() {
+        RacingJudge racingJudge = new RacingJudge();
+        return racingJudge.findWinner(cars);
+    }
 }
