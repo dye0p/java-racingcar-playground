6ae09bc52e49d5bf64b5841a92b5ef5226f52e4c
DongYeop <s01054830455@gmail.com>
2024-09-19 13:50:01 +0900

    refactor(InputView): 메서드 분리 리팩토링

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 import racingcar.exception.ErrorCode;
 
 public class InputView {
-    private static final String REGEX = ",";
+    private static final String SPLIT_PATTERN = ",";
     private static final String INPUT_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_RACING_COUNTS = "시도할 회수는 몇회인가요?";
     private static final int MIN_RACING_COUNT = 1;
-
 
     public String[] inputCarNames() {
         Scanner sc = new Scanner(System.in);
@@ -18,26 +17,47 @@ public class InputView {
     }
 
     private String[] inputSplit(Scanner sc) {
-        return sc.nextLine().split(REGEX);
+        return sc.nextLine().split(SPLIT_PATTERN);
     }
 
     public int inputRacingCount() {
         Scanner sc = new Scanner(System.in);
-        int racingCount = 0;
+
+        return inputAndValidateRacingCount(sc);
+    }
+
+    private int inputAndValidateRacingCount(Scanner sc) {
+        int racingCount;
 
         while (true) {
             System.out.println(INPUT_RACING_COUNTS);
             racingCount = sc.nextInt();
 
             try {
-                if (racingCount < MIN_RACING_COUNT) {
-                    throw new IllegalArgumentException(ErrorCode.INVALID_RACING_COUNT_MINIMUM.getMessage());
-                }
+                checkRacingCount(racingCount);
                 break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException exception) {
+                printExceptionMessage(exception);
             }
         }
         return racingCount;
+    }
+
+    private void printExceptionMessage(IllegalArgumentException exception) {
+        System.out.println(exception.getMessage());
+    }
+
+    private void checkRacingCount(int racingCount) {
+        if (!isRacingCount(racingCount)) {
+            throwInvalidRacingCountMinimumException();
+        }
+    }
+
+    private void throwInvalidRacingCountMinimumException() {
+        throw new IllegalArgumentException(ErrorCode.INVALID_RACING_COUNT_MINIMUM.getMessage());
+    }
+
+    private boolean isRacingCount(int racingCount) {
+        return racingCount > MIN_RACING_COUNT;
     }
 }
