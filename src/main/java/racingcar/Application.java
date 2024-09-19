721a4846b8b25be2a0f8c7ca65c9b11c61997b3d
DongYeop <s01054830455@gmail.com>
2024-09-19 10:39:19 +0900

    feat(Application): main 실행 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,4 +1,12 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+
 public class Application {
+    public static void main(String[] args) {
+        RacingGameController racingGameController = new RacingGameController();
+        racingGameController.readyRacing();
+        racingGameController.startRacing();
+        racingGameController.racingWinner();
+    }
 }
