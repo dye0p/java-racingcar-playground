11dfea3943b5fb69e0e8e05d64aa990bee3a2d92
DongYeop <s01054830455@gmail.com>
2024-09-19 10:33:26 +0900

    feat(RacingGameController): 경주 시작, 경주 결과, 우승자 찾기 기능 추가
    - 입력값에 따른 예외처리 로직 포함

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,4 +1,59 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.exception.ErrorCode;
+import racingcar.model.Car;
+import racingcar.model.Racing;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
 public class RacingGameController {
+
+    private final InputView inputView;
+    private final ResultView resultView;
+    private Racing racing;
+
+    public RacingGameController() {
+        inputView = new InputView();
+        resultView = new ResultView();
+    }
+
+    public void readyRacing() {
+        String carNames;
+        int racingCount;
+
+        while (true) {
+            try {
+                carNames = inputView.inputCarNames();
+                String[] splitCarNames = carNames.split(",");
+
+                for (String carName : splitCarNames) {
+                    if (carName.length() > 5) {
+                        throw new IllegalArgumentException(ErrorCode.CAR_NAME_LENGTH_EXCEEDED.getMessage());
+                    }
+                }
+                racingCount = inputView.inputRacingCount();
+
+                racing = new Racing(splitCarNames, racingCount);
+                break;
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void startRacing() {
+        resultView.racingResultInformation();
+        while (racing.isTryRacing()) {
+            racing.racing();
+            List<Car> cars = racing.racingResult();
+            resultView.racingResult(cars);
+        }
+    }
+
+    public void racingWinner() {
+        List<Car> winners = racing.winner();
+        resultView.resultWinner(winners);
+    }
 }
