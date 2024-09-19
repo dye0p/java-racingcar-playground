0b5b26f384b45952233df9b9216b0c01801d793e
DongYeop <s01054830455@gmail.com>
2024-09-19 10:31:55 +0900

    feat(RacingJudge): 우승자 판독 기능 추가

--- src/main/java/racingcar/model/RacingJudge.java
+++ src/main/java/racingcar/model/RacingJudge.java
@@ -1,4 +1,18 @@
 package racingcar.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RacingJudge {
+
+    public List<Car> findWinner(List<Car> cars) {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
 }
